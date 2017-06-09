037a0ce82b7ee88514704d898f25c71b5bcb270c
YuTanaka <tanaka@am1.jp>
2017-06-09 14:52:15 +0900

    8-6

--- move-labels/Form1.cs
+++ move-labels/Form1.cs
@@ -12,8 +12,8 @@ namespace move_labels
 {
     public partial class Form1 : Form
     {
-        int vx = -10;
-        int vy = -10;
+        int vx = -5;
+        int vy = -5;
         public Form1()
         {
             InitializeComponent();
@@ -25,19 +25,19 @@ namespace move_labels
             label1.Left += vx;
             label1.Top += vy;
             if (label1.Left < 0) {
-                vx = 10;
+                vx = Math.Abs(vx);
             }
             if (label1.Top < 0)
             {
-                vy = 10;
+                vy = Math.Abs(vy);
             }
             if (label1.Left > 477)
             {
-                vx = -10;
+                vx = -Math.Abs(vx);
             }
             if (label1.Top > 357)
             {
-                vy = -10;
+                vy = -Math.Abs(vy);
             }
         }
     }
