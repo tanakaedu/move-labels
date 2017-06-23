29bae71e8d9aab37b91b8e8edefe4cd7dc09bb5b
YuTanaka <tanaka@am1.jp>
2017-06-23 16:08:07 +0900

    ラベルをfor文で繰り返し完了

--- move-labels/Form1.Designer.cs
+++ move-labels/Form1.Designer.cs
@@ -36,8 +36,6 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -45,11 +43,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(53, 285);
+            this.label1.Location = new System.Drawing.Point(127, 38);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 27);
             this.label1.TabIndex = 0;
             this.label1.Text = "◆";
+            this.label1.Visible = false;
             // 
             // timer1
             // 
@@ -104,26 +103,6 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label6.Location = new System.Drawing.Point(157, 228);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(39, 27);
-            this.label6.TabIndex = 6;
-            this.label6.Text = "◆";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label7.Location = new System.Drawing.Point(274, 273);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(39, 27);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "◆";
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(12, 82);
@@ -140,8 +119,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 386);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -164,8 +141,6 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Button button2;
     }
 }
--- move-labels/Form1.cs
+++ move-labels/Form1.cs
@@ -12,11 +12,14 @@ namespace move_labels
 {
     public partial class Form1 : Form
     {
+        // const = 定数。プログラム実行時に値が変わらないもの
+        const int LABEL_COUNT = 50;
+
         // 0～2までで3つの値をもてる
-        int[] vx = new int[3];
-        int[] vy = new int[3];
+        int[] vx = new int[LABEL_COUNT];
+        int[] vy = new int[LABEL_COUNT];
         int iTime = 0;
-        Label[] labels = new Label[3];
+        Label[] labels = new Label[LABEL_COUNT];
 
         private static Random rand = new Random();
 
@@ -45,94 +48,37 @@ namespace move_labels
             label4.Top = cpos.Y - label4.Height / 2;
 
 
-            // label1.Left = label1.Left + vx;
-            label1.Left += vx[0];
-            label1.Top += vy[0];
-            label6.Left += vx[1];
-            label6.Top += vy[1];
-            label7.Left += vx[2];
-            label7.Top += vy[2];
+            for (int idx = 0; idx < LABEL_COUNT; idx++)
+            {
+                labels[idx].Left += vx[idx];
+                labels[idx].Top += vy[idx];
 
-            if (label1.Left < 0)
-            {
-                vx[0] = Math.Abs(vx[0]);
-            }
-            if (label1.Top < 0)
-            {
-                vy[0] = Math.Abs(vy[0]);
-            }
-            if (label1.Left > ClientSize.Width - label1.Width)
-            {
-                vx[0] = -Math.Abs(vx[0]);
-            }
-            if (label1.Top > ClientSize.Height - label1.Height)
-            {
-                vy[0] = -Math.Abs(vy[0]);
-            }
+                if (labels[idx].Left < 0)
+                {
+                    vx[idx] = Math.Abs(vx[idx]);
+                }
+                if (labels[idx].Top < 0)
+                {
+                    vy[idx] = Math.Abs(vy[idx]);
+                }
+                if (labels[idx].Left > ClientSize.Width - labels[idx].Width)
+                {
+                    vx[idx] = -Math.Abs(vx[idx]);
+                }
+                if (labels[idx].Top > ClientSize.Height - labels[idx].Height)
+                {
+                    vy[idx] = -Math.Abs(vy[idx]);
+                }
 
+                if ((cpos.X > labels[idx].Left)
+    && (cpos.X < labels[idx].Right)
+    && (cpos.Y > labels[idx].Top)
+    && (cpos.Y < labels[idx].Bottom)
+    )
+                {
+                    labels[idx].Visible = false;
+                }
 
-            if (label6.Left < 0)
-            {
-                vx[1] = Math.Abs(vx[1]);
-            }
-            if (label6.Top < 0)
-            {
-                vy[1] = Math.Abs(vy[1]);
-            }
-            if (label6.Left > ClientSize.Width - label6.Width)
-            {
-                vx[1] = -Math.Abs(vx[1]);
-            }
-            if (label6.Top > ClientSize.Height - label6.Height)
-            {
-                vy[1] = -Math.Abs(vy[1]);
-            }
-
-
-            if (label7.Left < 0)
-            {
-                vx[2] = Math.Abs(vx[2]);
-            }
-            if (label7.Top < 0)
-            {
-                vy[2] = Math.Abs(vy[2]);
-            }
-            if (label7.Left > ClientSize.Width - label7.Width)
-            {
-                vx[2] = -Math.Abs(vx[2]);
-            }
-            if (label7.Top > ClientSize.Height - label7.Height)
-            {
-                vy[2] = -Math.Abs(vy[2]);
-            }
-
-
-            if ((cpos.X > label1.Left)
-                && (cpos.X < label1.Right)
-                && (cpos.Y > label1.Top)
-                && (cpos.Y < label1.Bottom)
-                )
-            {
-                label1.Visible = false;
-            }
-
-
-            if ((cpos.X > label6.Left)
-                && (cpos.X < label6.Right)
-                && (cpos.Y > label6.Top)
-                && (cpos.Y < label6.Bottom)
-                )
-            {
-                label6.Visible = false;
-            }
-
-            if ((cpos.X > label7.Left)
-                && (cpos.X < label7.Right)
-                && (cpos.Y > label7.Top)
-                && (cpos.Y < label7.Bottom)
-                )
-            {
-                label7.Visible = false;
             }
 
         }
@@ -144,7 +90,7 @@ namespace move_labels
             // vx,vyに乱数を求める
             // 冗長（じょうちょう）だったコードをまとめることができた
             // 冗長＝同じものが繰り返される様
-            for (int idx = 0; idx < 3; idx++)
+            for (int idx = 0; idx < LABEL_COUNT; idx++)
             {
                 vx[idx] = rand.Next(-10, 11);
                 vy[idx] = rand.Next(-10, 11);
@@ -153,6 +99,7 @@ namespace move_labels
                 labels[idx] = new Label();
                 labels[idx].AutoSize = true;  // 幅と高さを自動制御
                 labels[idx].Text = "◆";
+                labels[idx].Font = label1.Font;
                 // フォームに配置
                 Controls.Add(labels[idx]);
 
